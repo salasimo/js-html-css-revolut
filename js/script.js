fa79cef15185920fb2da0731d8dc16aedec10f10
Simone Sala <simonesala@me.com>
2020-02-28 15:26:22 +0000

    Controllo menu con logica
    Un solo menu drop-down rimane aperto

--- js/script.js
+++ js/script.js
@@ -3,8 +3,15 @@ $(document).ready(function() {
 // Il mio codice jQuery:
 
 $(".menu-item a").click(function(){
-    $(this).parent(".menu-item").children(".dropdown-menu").slideToggle(300);
+    if ($(this).parent(".menu-item").children(".dropdown-menu").is(":visible")){
+        $(this).parent(".menu-item").children(".dropdown-menu").slideToggle(300);
+    } else {
+        $(".dropdown-menu").slideUp(300);
+        $(this).parent(".menu-item").children(".dropdown-menu").slideToggle(300);
+    }
+
 });
+
 
 
 
